fccd453b613b7b695d9729fa8baa3fea405a9dd7
maximrakov <78982680+maximrakov@users.noreply.github.com>
2022-12-11 16:52:50 +0300

    implement lab-7

--- scripts/preprocessing.js
+++ scripts/preprocessing.js
@@ -1,6 +1,11 @@
+
 (() => {
     const start = Date.now()
     window.addEventListener("load", () => {
+        // Достаем header и инициализируем желаемое поведение на нем с помощью модуля
+        const header = document.querySelector("header");
+        const headroom = new Headroom(header);
+        headroom.init();
         const finish = Date.now()
         const el = document.querySelector(".loadtime")
         el.innerText = `${finish - start} ms`
@@ -23,24 +28,28 @@
         const currentPage = document.getElementById(page);
         console.log(page);
         if(page == 'aboutMe') {
+            //вызываем метод выдающий уведомление пользователю
             iziToast.show({
                 title: '',
                 message: 'This is information about me'
             });
         }
         if(page == 'contacts') {
+            //вызываем метод выдающий уведомление пользователю
             iziToast.show({
                 title: '',
                 message: 'In this page you can find my contact'
             });
         }
         if(page == 'myProjects') {
+            //вызываем метод выдающий уведомление пользователю
             iziToast.show({
                 title: '',
                 message: 'There are my projects'
             });
         }
         if(page === '' || page === 'index') {
+            //вызываем метод выдающий уведомление пользователю
             iziToast.show({
                 title: 'Hello',
                 message: 'Welcome to my site!'
